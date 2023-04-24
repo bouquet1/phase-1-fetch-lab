3f6ee4d09d2c611a7c76e2c76281522906ff26cd
bouquet1 <bukuleta@gmail.com>
2023-04-23 20:33:20 -0700

    fetched

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => console.log(json));
 }
 
 function renderBooks(books) {
